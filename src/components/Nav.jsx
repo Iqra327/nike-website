67d84b0f1f007a41defb2d11e0ff1ab08d94a7d7
Iqra327 <iqrarasheed219@gmail.com>
2024-09-23 00:57:31 +0500

    initial-commit

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,6 @@
 import { headerLogo } from '../assets/images';
-import { hamburger } from '../assets/icons'
+import { hamburger } from '../assets/icons';
+import { navLinks } from '../constants';
 const Nav = () => {
   return (
     <header className='padding-x py-8 absolute z-10 w-full'>
@@ -12,6 +13,28 @@ const Nav = () => {
             height={29} 
           />
         </a>
+        <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+          {
+            navLinks.map((item) => (
+              <li key={item.id}>
+                <a 
+                  href={item.href}
+                  className='font-montserrat leading-normal text-lg text-slate-gray'
+                  >
+                    {item.label}
+                </a>
+              </li>
+            ))
+          }
+        </ul>
+        <div className='hidden max-lg:block'>
+          <img 
+            src={hamburger}
+            alt='Hamburger'
+            width={25} 
+            height={25}
+          /> 
+        </div>
       </nav>
     </header>
   )
